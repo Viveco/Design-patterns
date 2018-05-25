579c67e8b1de3d94f6f2ca526fdc8bc67c7448e3
yuangong <shesay123@sina.com>
2018-05-25 18:52:17 +0800

    aa
    aa

--- 设计模式/ViewController.m
+++ 设计模式/ViewController.m
@@ -40,6 +40,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"aa");
     
     _dataSource = @[@"设计模式",@"SKU 刷选",@"View的动画",@"一些东西，什么通知，数据持久化等",@"生命周期等",@"NSTimer-GCD等",@"RunLoop-RunTime",@"网络请求等",@"加密问题",@"地图",@"推送",@"传感器等",@"数据存储",@"runloop 使用场景"];
     
